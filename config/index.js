5eed89371ec6d1c5be7b3b575d082a78871c2792
OriHaguel <orihaguel@gmail.com>
2024-09-15 19:22:15 +0300

    Add build

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
 import configProd from './prod.js'
-// import configDev from './dev.js'
+import configDev from './dev.js'
 
 
 export var config
-config = configProd
-// if (process.env.NODE_ENV === 'production') {
-//   config = configProd
-// } else {
-//   config = configDev
-// }
+// config = configProd
+if (process.env.NODE_ENV === 'production') {
+    config = configProd
+} else {
+    config = configDev
+}
 config.isGuestMode = true
 
 
