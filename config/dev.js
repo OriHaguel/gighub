d70691f79ce4338fe4d3f9eaeacaef78c2ecf016
OriHaguel <orihaguel@gmail.com>
2024-10-05 17:55:02 +0300

    Add env

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: 'mongodb+srv://orihaguel:3ikfWD2DtOe956Zf@cluster0.y9nmq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  dbURL: '',
   dbName: 'gighub_db'
 }
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://orihaguel:3ikfWD2DtOe956Zf@cluster0.y9nmq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  dbName: process.env.DB_NAME || 'gighub_db'
+  dbURL: process.env.MONGO_URL || '',
+  dbName: process.env.DB_NAME || ''
 }
